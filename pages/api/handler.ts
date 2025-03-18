8d165a69855605341a65d69b32e9a27cfdf3d8ea
Prateek Bhatnagar <prateek89born@gmail.com>
2025-03-18 16:03:00 +0000

    add new redirect url

--- pages/api/handler.ts
+++ pages/api/handler.ts
@@ -8,12 +8,12 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-    
+  const redirectURL = `https://accounts.google.com/o/oauth2/v2/auth${req.url?.substring(req.url.indexOf("?"))}`
   res.status(200).json({
     // @ts-expect-error
     method: req.method,
     body: req.body,
     qs: req.query,
-    url: req.url
+    redirectURL
   })
 }
