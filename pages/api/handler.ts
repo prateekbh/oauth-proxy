f9f4b95c62beb5ba250b9197f6c1c22234887ada
Prateek Bhatnagar <prateek89born@gmail.com>
2025-03-28 14:53:29 +0000

    fix redirection

--- pages/api/handler.ts
+++ pages/api/handler.ts
@@ -9,13 +9,13 @@ export default function handler(
   res: NextApiResponse<ResponseData>
 ) {
   const redirectURL = `https://accounts.google.com/o/oauth2/v2/auth${req.url?.substring(req.url.indexOf("?"))}&prompt=select_account`
-  // res.redirect(redirectURL);
-  res.status(200).json({
-    // @ts-expect-error
-    method: req.method,
-    body: req.body,
-    qs: req.query,
-    url: req.url,
-    redirectURL  
-  });
+  res.redirect(redirectURL);
+  // res.status(200).json({
+  //   // @ts-expect-error
+  //   method: req.method,
+  //   body: req.body,
+  //   qs: req.query,
+  //   url: req.url,
+  //   redirectURL  
+  // });
 }
